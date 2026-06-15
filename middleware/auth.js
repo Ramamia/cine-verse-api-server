caea8f1475633de3f019fc21e9cec766f5a7d4ed
ramamia <rama.selwadi04@gmail.com>
2026-06-16 01:13:20 +0300

    some small fixes and i tested with postman and i changed my comments a bit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const authenticateToken = (req, res, next) => {
-    // Look for the Authorization header: "Bearer <token>"
+    // listen we need to check if they have a token in the headers yk
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
@@ -11,9 +11,9 @@ const authenticateToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        // Attach the decoded user data (like user id) to the request object
+        // okay so we attach the user info to the request so we know who it is
         req.user = decoded;
-        next(); // Proceed to the next middleware or route handler
+        next(); 
     } catch (error) {
         return res.status(403).json({ error: "Invalid token." });
     }
